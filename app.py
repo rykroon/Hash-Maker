445223a057b58652d116555abb8852dec5a0c2ec
Ryan Kroon <rykroon.tech@gmail.com>
2020-02-23 20:11:38 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from base64 import urlsafe_b64encode as b64encode
+from hashlib import md5, sha1, sha256, sha512
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/hash', methods=['GET'])
+def get_hash():
+    q = request.args.get('q')
+    if q is None:
+        return jsonify(error="missing 'q' parameter.")
+
+    if q is not None:
+        md5_hash = md5(q.encode())
+        sha1_hash = sha1(q.encode())
+        sha256_hash = sha256(q.encode())
+        sha512_hash = sha512(q.encode())
+        d = {
+            'md5': {
+                'hex': md5_hash.hexdigest(),
+                'base64': b64encode(md5_hash.digest()).decode()
+            },
+            'sha1':{
+                'hex': sha1_hash.hexdigest(),
+                'base64': b64encode(sha1_hash.digest()).decode()
+            },
+            'sha256': {
+                'hex': sha256_hash.hexdigest(),
+                'base64': b64encode(sha256_hash.digest()).decode() 
+            },
+            'sha512' : {
+                'hex': sha512_hash.hexdigest(),
+                'base64': b64encode(sha512_hash.digest()).decode()
+            }
+        }
+        return jsonify(d)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
