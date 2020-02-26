392348c3cba50bfedb649864958e45f013187ba4
Ryan Kroon <rykroon.tech@gmail.com>
2020-02-26 12:59:59 -0500

    made code more efficient. renmoved duplicates

--- src/blueprints/randomwords.py
+++ src/blueprints/randomwords.py
@@ -6,31 +6,40 @@ word_bp = Blueprint('random-words', __name__)
 
 stopwords_set = set(stopwords.words('english'))
 brown_set = set(brown.words())
-brown_set = brown_set.difference(stopwords_set)
 
-global_words = list(brown_set)
-global_words = [w for w in global_words if w.isalpha()]
+diff_set = brown_set.difference(stopwords_set)
+diff_set = {e.lower() for e in diff_set if e.isalpha()}
+
+global_words = list(diff_set)
 
 @word_bp.route('/', strict_slashes=False)
 def get_random_words():
     word_length = int(request.args.get('word_length', 0))
     num_of_words = int(request.args.get('num_of_words', 4))
     num_of_words = min(num_of_words, 12)
-    starts_with = request.args.get('starts_with')
+    starts_with = request.args.get('starts_with', '').lower()
     
     words = global_words
 
-    if word_length > 0:
-        words = [w for w in words if len(w) == word_length]
+    filter_ = None
+    if word_length and starts_with:
+        filter_ = lambda w: len(w) == word_length and w.startswith(starts_with)
+    elif word_length:
+        filter_ = lambda w: len(w) == word_length
+    elif starts_with:
+        filter_ = lambda w: w.startswith(starts_with)
+    
+    if filter_:
+        words = [w for w in words if filter_(w)]
+    
+    if len(words) < num_of_words:
+        return jsonify(words)
 
-    if starts_with:
-        words = [w for w in words if w.startswith(starts_with)]
-
-    result = []
-    for _ in range(num_of_words):
+    result = set()
+    while len(result) < num_of_words:
         idx = randint(0, len(words) - 1)
-        result.append(words[idx])
+        result.add(words[idx])
 
-    return jsonify(result)
+    return jsonify(list(result))
 
 
