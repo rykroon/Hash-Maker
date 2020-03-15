0a97bb6d47c6813b8565f130d8382d1243b77ee2
Ryan Kroon <rykroon.tech@gmail.com>
2020-03-15 17:56:09 -0400

    created a home page

--- src/app.py
+++ src/app.py
@@ -11,6 +11,31 @@ app.register_blueprint(hash_bp, url_prefix='/hash')
 app.register_blueprint(word_bp, url_prefix='/random-words')
 app.register_blueprint(nounphrase_bp, url_prefix='/noun-phrase')
 
+@app.route('/', methods=['GET'])
+def root():
+    return jsonify({
+        'endpoints': {
+            '/hash': {
+                'params': {
+                    'q': 'The string to be hashed'
+                }
+            },
+            '/random-words': {
+                'params': {
+                    'num_of_words': 'The number of words to be returned',
+                    'word_length': 'The length of the words',
+                    'starts_with': 'A substring that the words should start with'
+                }
+            },
+            '/noun-phrase': {
+                'params': {
+                    'starts_with': 'A substring that the adjective and noun should start with.'
+                }
+            }
+        }
+    })
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
 
